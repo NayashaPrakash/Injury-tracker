755881108113a19283eade91d14d7e1a35547e99
NayashaPrakash <Nayashap01@gmail.com>
2023-10-27 11:34:22 +0530

    Update api.js

--- frontend/src/app/views/injcomp/forms/api.js
+++ frontend/src/app/views/injcomp/forms/api.js
@@ -6,7 +6,7 @@ export const saveFormDataToMongoDB = async (formData) => {
   };
   console.log(formData);
   try {
-    const response = await fetch('https://injurytracker.vercel.app/api/saveFormData', {
+    const response = await fetch('https://injury-tracker-backend.vercel.app/api/saveFormData', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
