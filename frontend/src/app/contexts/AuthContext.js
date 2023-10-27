c403cc0645ff687d9e35ecc48559586dc50a44ba
NayashaPrakash <Nayashap01@gmail.com>
2023-10-27 17:44:01 +0530

    Update AuthContext.js

--- frontend/src/app/contexts/AuthContext.js
+++ frontend/src/app/contexts/AuthContext.js
@@ -79,9 +79,11 @@ export const AuthProvider = ({ children }) => {
     try {
       const response = await axios.post('https://injury-tracker-backend-blond.vercel.app/api/login', { email, password },  { withCredentials: true });
       const { user, token } = response.data;
+      console.log('[[[[[[[[[[[[['+ responsse.data);
 
       dispatch({ type: 'LOGIN', payload: { user, accessToken: token } });
     } catch (error) {
+      console.log('-------');
       console.error('Login failed:', error);
     }
   };
