b9c82a2fe8b56a4f87a4048380a37d90e7943aba
NayashaPrakash <Nayashap01@gmail.com>
2023-10-27 11:33:53 +0530

    Update AuthContext.js apis

--- frontend/src/app/contexts/AuthContext.js
+++ frontend/src/app/contexts/AuthContext.js
@@ -75,7 +75,7 @@ export const AuthProvider = ({ children }) => {
 
   const login = async (email, password) => {
     try {
-      const response = await axios.post('https://injurytracker.vercel.app/api/login', { email, password });
+      const response = await axios.post('https://injury-tracker-backend.vercel.app//api/login', { email, password });
       const { user, token } = response.data;
 
       dispatch({ type: 'LOGIN', payload: { user, accessToken: token } });
@@ -86,7 +86,7 @@ export const AuthProvider = ({ children }) => {
 
   const register = async (email, username, password) => {
     try {
-      const response = await axios.post('https://injurytracker.vercel.app/api/register', {
+      const response = await axios.post('https://injury-tracker-backend.vercel.app//api/register', {
         email,
         username,
         password
@@ -101,7 +101,7 @@ export const AuthProvider = ({ children }) => {
 
   const getForms = async () => {
     try {
-      const response = await axios.get('https://injurytracker.vercel.app/api/myForms', {
+      const response = await axios.get('https://injury-tracker-backend.vercel.app//api/myForms', {
         withCredentials: true
       });
       const forms = response.data;
