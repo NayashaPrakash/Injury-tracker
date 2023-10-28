fab68185916c5a9f99b111b6d91292747611a37b
NayashaPrakash <Nayashap01@gmail.com>
2023-10-28 11:03:50 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,9 +1,9 @@
 // config.js (create a new file for configuration)
 
-const crypto = require('crypto');
-const secretKey = crypto.randomBytes(32).toString('base64');
-console.log(secretKey);
+// const crypto = require('crypto');
+// const secretKey = crypto.randomBytes(32).toString('base64');
+// console.log(secretKey);
 
-module.exports = {
-  jwtSecretKey: secretKey
-};
+// module.exports = {
+//   jwtSecretKey: secretKey
+// };
